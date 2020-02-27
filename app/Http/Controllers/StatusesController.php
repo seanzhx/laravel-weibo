2e7707724e8b8b06493a0ed83448fe53c0792331
seanzx <xingkid@gmail.com>
2020-02-27 04:26:46 +0000

    delete status

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -30,6 +30,9 @@ class StatusesController extends Controller
 
     public function destroy(Status $status)
     {
-
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
+        return redirect()->back();
     }
 }
